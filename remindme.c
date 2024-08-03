5c8b81fa5be2236f6de5253a84d520576e51f0bb
carrotfarmer <dhruvas17068@gmail.com>
2024-08-03 14:18:26 -0700

    basic arg parsing

--- remindme.c
+++ remindme.c
@@ -1,6 +1,36 @@
+#include <stdbool.h>
 #include <stdio.h>
 
-int main() {
-  printf("Hello, World!\n");
-  return 0;
+#define EXIT_SUCCESS 0
+#define EXIT_ERR_TOO_FEW_ARGS 1
+
+struct Reminder {
+  unsigned short id;
+  char *message;
+  // TODO: better type
+  char *time;
+};
+
+int main(int argc, char **argv) {
+  bool create = false;
+
+  if (argc < 2) {
+    fprintf(stderr, "Usage: %s <message>\n", argv[0]);
+    return EXIT_ERR_TOO_FEW_ARGS;
+  }
+
+  if (argc == 3) {
+    create = true;
+
+    struct Reminder r;
+    r.message = argv[1];
+    r.id = 0;
+    r.time = argv[2];
+
+    printf("Reminder: %s\n", r.message);
+    printf("Time: %s\n", r.time);
+    printf("ID: %d\n", r.id);
+  }
+
+  return EXIT_SUCCESS;
 }
